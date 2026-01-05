a42142ffbf2349f2fb9e25017c17d84f4a11b1a9
Abhijith <abhijith.nr.18@gmail.com>
2026-01-05 10:47:03 +0530

    TUF first commit

---
+++ basic_program.py
@@ -0,0 +1,13 @@
+"Complete the function printNumber which takes an integer input from the user and prints it on the screen."
+
+
+class Solution:
+    def printNumber(self):
+
+        number = int(input("Enter a number : "))
+
+        print(number)
+
+solution_instance = Solution()
+
+solution_instance.printNumber()
